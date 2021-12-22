ff91ab8eda531b3fce9ec7dcf0a35c88f9af7efc
Alban Sagouis <alban.sagouis@idiv.de>
2021-12-22 18:35:32 +0100

    references update

--- R/3.0_references.r
+++ R/3.0_references.r
@@ -1,4 +1,9 @@
 # Preparing a reference list for all data sets ----
 ## the citation for the data repository and the article are associated when available.
 
-   references <- data.table::fread(file = "./data/list_of_datasets.csv", na.strings = c("", "NA"), encoding = "UTF-8", select = c("dataset_id","reference"))
+references <- data.table::fread(
+  file = "./data/list_of_datasets.csv",
+  na.strings = c("", "NA"),
+  encoding = "UTF-8",
+  select = c("dataset_id","reference")
+)
