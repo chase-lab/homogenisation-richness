f8c2087c3e9a214421440e7b77ac70891f84e6f6
Alban Sagouis <alban.sagouis@idiv.de>
2021-12-22 11:04:07 +0100

    list of datasets update - homogenisation of values and references

---
+++ R/3.0_references.r
@@ -0,0 +1,4 @@
+# Preparing a reference list for all data sets ----
+## the citation for the data repository and the article are associated when available.
+
+   references <- data.table::fread(file = "./data/list_of_datasets.csv", na.strings = c("", "NA"), encoding = "UTF-8", select = c("dataset_id","reference"))
